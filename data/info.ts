25f18ba1734030dab3f50a62f9c89d78bac69aa5
Ahmet Can Altıntaş <153134765+ahmetcnaltintas@users.noreply.github.com>
2025-07-31 17:34:30 +0300

    Update Adress

--- data/info.ts
+++ data/info.ts
@@ -2,7 +2,7 @@ export const info = {
   phone: "0530 854 88 16",
   tel: "05308548816",
   whatsapp: "905308548816",
-  email: "atesotokurtarma16@gmail.com",
+  email: "info@bursayakincekici.com",
   address: "Bursa, Türkiye",
-  footerContent: "Yolda kaldığınızda hızlı, güvenli ve profesyonel destekle yanınızdayız. Bursa'nın her noktasına anında müdahale ile Ateş Oto Kurtarma hizmetinizde.",
+  footerContent: "Ateş Oto Kurtarma olarak Bursa ve çevre illerde (Yalova, Eskişehir, Balıkesir, Kütahya) 7/24 hızlı, güvenilir ve profesyonel oto kurtarma, yol yardımı ve çekici hizmetleri sunuyoruz. Acil durumlarda en yakın çekici için hemen bizi arayın!",
 };
